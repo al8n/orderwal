598a6145249d3b9ff5cb21700d496113146129eb
Al Liu <scygliu1@gmail.com>
2024-09-12 02:22:28 +0800

    Fix miri CI

--- ci/miri_tb_generic.sh
+++ ci/miri_tb_generic.sh
@@ -5,6 +5,6 @@ rustup toolchain install nightly --component miri
 rustup override set nightly
 cargo miri setup
 
-export MIRIFLAGS="-Zmiri-strict-provenance -Zmiri-symbolic-alignment-check -Zmiri-disable-isolation -Zmiri-tree-borrows"
+export MIRIFLAGS="-Zmiri-symbolic-alignment-check -Zmiri-disable-isolation -Zmiri-tree-borrows"
 
 cargo miri test --tests --all-features
