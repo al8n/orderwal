410d354270ff41d5ef8fdcdf85e817eac21edab9
Al Liu <scygliu1@gmail.com>
2024-09-14 02:09:25 +0800

    Update tests.rs

--- src/unsync/tests.rs
+++ src/unsync/tests.rs
@@ -7,8 +7,3 @@ use super::*;
 const MB: u32 = 1024 * 1024;
 
 common_unittests!(unsync::OrderWal);
-
-#[test]
-fn test_last_inmemory1() {
-  last(&mut OrderWal::new(Builder::new().with_capacity(MB)).unwrap());
-}
