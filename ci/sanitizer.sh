811832a8f2f9dbd6461d1bbaa1aeb85c2b87f1f9
Al Liu <scygliu1@gmail.com>
2024-09-14 22:19:29 +0800

    Update tsan CI

--- ci/sanitizer.sh
+++ ci/sanitizer.sh
@@ -6,12 +6,14 @@ export ASAN_OPTIONS="detect_odr_violation=0 detect_leaks=0"
 
 # Run address sanitizer
 RUSTFLAGS="-Z sanitizer=address" \
-cargo test --tests --target x86_64-unknown-linux-gnu --all-features
+cargo test -Z build-std --all --release --tests --target x86_64-unknown-linux-gnu --all-features --exclude benchmarks -- --test-threads=1
 
-# Run leak sanitizer
-RUSTFLAGS="-Z sanitizer=leak" \
-cargo test --tests --target x86_64-unknown-linux-gnu --all-features
+# Run memory sanitizer
+RUSTFLAGS="-Z sanitizer=memory" \
+cargo test -Z build-std --all --release --tests --target x86_64-unknown-linux-gnu --all-features --exclude benchmarks -- --test-threads=1
 
 # Run thread sanitizer
-RUSTFLAGS="-Z sanitizer=thread" \
-cargo -Zbuild-std test --tests --target x86_64-unknown-linux-gnu --all-features
+cargo clean
+TSAN_OPTIONS="suppressions=$(pwd)/ci/tsan" \
+RUSTFLAGS="${RUSTFLAGS:-} -Z sanitizer=thread" \
+    cargo test -Z build-std --all --release --target x86_64-unknown-linux-gnu --all-features --tests --exclude benchmarks -- --test-threads=1
